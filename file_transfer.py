8d8ff34da3203340c08fc069c898913f91bbbf3b
matias <mmerlog@fi.uba.ar>
2022-05-25 19:01:19 -0300

    Hecho el upload Co-authored-by: MauricioBuzzone <mbuzzone@fi.uba.ar>

--- file_transfer.py
+++ file_transfer.py
@@ -1,11 +1,16 @@
+from fileinput import filename
 from socket import socket, AF_INET, SOCK_DGRAM, SOCK_STREAM 
 from threading import Thread
 import logging
 import struct
+import os
 
 
 class file_transfer:
-    #path = path donde vamos a dejar los files
+    dir_path = "server_files"
+    UPLOAD = 0
+    DOWNLOAD = 1
+    MSS = 1500 
 
    
     def start_server(self):
@@ -13,6 +18,11 @@ class file_transfer:
         server_socket = socket(AF_INET,SOCK_STREAM)
         server_socket.bind('',12000)
         server_socket.listen()
+        #Creamos directorio
+        try:
+            os.mkdir(self.dir_path)
+        except:
+            pass
 
         while True:
             connSocket , addr = server_socket.accept()
@@ -25,10 +35,7 @@ class file_transfer:
         return
 
     def client_handle(self,connSocket, addr):
-        # receive: tuki
-        # decode
-        # llamar a receive_file o a send_file
-        CONFIG_LEN = 205
+        CONFIG_LEN = 209
         config_msg = []
         bytes_totales_recibidos = 0
      
@@ -38,14 +45,63 @@ class file_transfer:
             bytes_totales_recibidos += len(bytes_recibidos)
 
         op = struct.unpack("c",config_msg[0])
-        file_len = struct.unpack("i",config_msg[1:5])
-        file_name = config_msg[5:]
+        file_size = struct.unpack("q",config_msg[1:9])
+        file_name = config_msg[9:]
 
+        if op == self.UPLOAD:
+            logging.debug("Operación es RECEIVE")
+            self.upload_file(connSocket,file_name,file_size)
+        elif op == self.DOWNLOAD:
+            logging.debug("Operación es SEND")
+            self.download_file(connSocket,file_name)
+        else:
+            logging.debug("Mensaje inválido")
+            connSocket.close()
+            raise RuntimeError("Operación no tiene un valor válido")
         return
 
-    def receive_file(self):
+    def upload_file(self,connSocket,file_name,file_size):
+        #Creamos el file, si ya existe lo sobreescribimos. 
+        file_path = self.dir_path + "/" + file_name
+        f = open(file_path,"wb")
+
+        SIZE = file_size
+        bytes_totales_recibidos = 0
+     
+        logging.debug("Iniciando upload")
+        while bytes_totales_recibidos < SIZE: 
+            bytes_recibidos = connSocket.recv(self.MSS)
+            
+            #Se cerró el socket antes de terminar el upload
+            if bytes_recibidos == b'':
+                logging.debug("Se cerró el socket antes de terminar el upload, Socket ID:{}".format(connSocket))
+                connSocket.close()
+                os.remove(file_path)
+            
+            f.write(bytes_recibidos)
+            bytes_totales_recibidos += len(bytes_recibidos)
+
+        logging.debug("Terminado el upload de: {}".format(file_name))
+        connSocket.close()
         return
 
-    def send_file(self):
+    def download_file(self,connSocket,file_name,file_size):
+
+        
+        file = []
+        bytes_totales_recibidos = 0
+     
+        logging.debug("Iniciando download")
+        while bytes_totales_recibidos < SIZE: 
+            bytes_recibidos = connSocket.recv(self.MSS)
+            
+            #Se cerró el socket antes de terminar el upload
+            if bytes_recibidos == b'':
+                logging.debug("Se cerró el socket antes de terminar el upload, Socket ID:{}".format(connSocket))
+                connSocket.close()
+
+            file.append(bytes_recibidos)
+            bytes_totales_recibidos += len(bytes_recibidos)
+
         return
     
